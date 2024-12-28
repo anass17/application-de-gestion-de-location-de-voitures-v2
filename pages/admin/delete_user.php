33bab79d9bec26061279e074d70b95a016036a52
anass17 <anassboutaib2018@gmail.com>
2024-12-28 01:06:29 +0100

    Added delete user functionality

---
+++ pages/admin/delete_user.php
@@ -0,0 +1,33 @@
+<?php
+
+    session_start();
+
+    require_once '../../vendor/autoload.php';
+    require '../../connection/db.php';
+    require '../../auth/UserClass.php';
+    require '../class/carClass.php';
+    require '../class/RentalContractClass.php';
+
+    if (isset($_GET['id'])) {
+        $clientId = $_GET['id'];
+
+        $conn = (new Database) -> connect();
+
+        $user = new User($conn);
+        $user -> fetchById($clientId);
+
+        
+
+        $sql = "DELETE FROM Clients WHERE id = $clientId";
+
+        if ($user -> delete()) {
+            header("Location: view_users.php");
+            exit;
+        } else {
+            echo "Error! " . $user -> error;
+        }
+    } else {
+        echo "Client ID is missing.";
+    }
+
+?>
